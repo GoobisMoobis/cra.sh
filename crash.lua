a7425ced13104b93c8da8e4ad18c9048e65e31d3
GoobisMoobis <141375174+GoobisMoobis@users.noreply.github.com>
2025-10-26 00:28:14 -0400

    RIP Flumi
    Added a function to append new elements to selected divs.

--- crash.lua
+++ crash.lua
@@ -2,3 +2,17 @@
 ---@diagnostic disable: keyword
 -- #selene:allow(undefined_variable)
 
+local function hehe(list)
+  if list ~= nil then
+    for i = 1, #list do
+      local element = list[i]
+      local newElement = gurt.create('div')
+      element:append(newElement)
+    end
+  end
+end
+
+while true do
+  local crashers = gurt.select('div')
+  hehe(crashers)
+end
